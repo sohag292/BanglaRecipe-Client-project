4cfe461cd4d9648434d6a594212ad06a79d0c83c
sohag292 <sohagshah571@gmail.com>
2023-05-04 15:58:01 +0600

    lazy load is added

--- src/components/CheifProfileCard/CheifProfileCard.jsx
+++ src/components/CheifProfileCard/CheifProfileCard.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
+import LazyLoad from 'react-lazy-load';
 
 export default function CheifProfileCard({ profile }) {
     console.log(profile);
     return (
         <Col md={6}>
             <Card>
-                <div className="text-center my-4"><img className="img-fluid" src={profile.image} /></div>
+                    <LazyLoad className=' my-4 mx-auto'  height={300} width={300} threshold={0.95}>
+                        <img className="img-fluid" src={profile.image} alt="Chef" />
+                    </LazyLoad>
+
                 <Card.Body>
                     <h3>Name: {profile.name}</h3>
                     <h5>Experience: {profile.experience}</h5>
@@ -20,8 +24,8 @@ export default function CheifProfileCard({ profile }) {
                         <div><button className="btn btn-primary">View Recipes</button></div>
                     </div>
                 </Card.Body>
-        </Card>
-</Col >
+            </Card>
+        </Col >
 
 
     )
